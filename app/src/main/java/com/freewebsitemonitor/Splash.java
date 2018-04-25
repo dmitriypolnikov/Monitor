c017d1b76dc4273a4b99ec74413bac3c8a89bb2d
Dmitriy Polnikov <Dmitriy Polnikov>
2018-04-25 04:08:06 -0700

    update

--- app/src/main/java/com/freewebsitemonitor/Splash.java
+++ app/src/main/java/com/freewebsitemonitor/Splash.java
@@ -31,23 +31,23 @@ public class Splash extends AppCompatActivity implements GlobalKeys {
         setContentView(R.layout.activity_splash);
         MyApp.component().inject(this);
 
-//        PackageInfo info;
-//		try {
-//		    info = getPackageManager().getPackageInfo("com.freewebsitemonitor", PackageManager.GET_SIGNATURES);
-//		    for (Signature signature : info.signatures) {
-//		        MessageDigest md;
-//		        md = MessageDigest.getInstance("SHA");
-//		        md.update(signature.toByteArray());
-//		        String something = new String(Base64.encode(md.digest(), 0));
-//		        Log.e("hash key", something);
-//		    }
-//		} catch (PackageManager.NameNotFoundException e1) {
-//		    Log.e("name not found", e1.toString());
-//		} catch (NoSuchAlgorithmException e) {
-//		    Log.e("no such an algorithm", e.toString());
-//		} catch (Exception e) {
-//		    Log.e("exception", e.toString());
-//		}
+        PackageInfo info;
+		try {
+		    info = getPackageManager().getPackageInfo("com.freewebsitemonitor", PackageManager.GET_SIGNATURES);
+		    for (Signature signature : info.signatures) {
+		        MessageDigest md;
+		        md = MessageDigest.getInstance("SHA");
+		        md.update(signature.toByteArray());
+		        String something = new String(Base64.encode(md.digest(), 0));
+		        Log.e("hash key", something);
+		    }
+		} catch (PackageManager.NameNotFoundException e1) {
+		    Log.e("name not found", e1.toString());
+		} catch (NoSuchAlgorithmException e) {
+		    Log.e("no such an algorithm", e.toString());
+		} catch (Exception e) {
+		    Log.e("exception", e.toString());
+		}
 
         new Thread() {
             public void run() {
